0396df011373f884d6ca89c7e2df2504dbe70720
sabbaghh <afsabbagh9@gmail.com>
2024-11-04 16:41:35 +0400

    Add Suspense

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 import Hero from '@/components/ProjectsPage/Hero';
 import ItemList from '@/components/ProjectsPage/ItemList';
 import Filters from '@/components/ProjectsPage/Filters';
-
+import { Suspense } from 'react';
 export default async function Home() {
   return (
     <>
       <Hero />
       <Filters />
-      <ItemList />
+      <Suspense fallback={<>loading..</>}>
+        <ItemList />
+      </Suspense>
     </>
   );
 }
